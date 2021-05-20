e2a8db55e3259e8070b684c35a2324cdbf2653b5
Dong Dao <kwalitycontrol@gmail.com>
2021-05-20 21:54:44 +0900

    add 404 page

--- main.go
+++ main.go
@@ -11,7 +11,11 @@ func handleFunc(w http.ResponseWriter, r *http.Request){
 		fmt.Fprint(w, "<h1>Welcome to my super awesome site</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:suppoer@lenslockedlcom\">support@lenslock.com</a>")
-	} 
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, `<h1>We coudld not find the page you are looking for :(</h1>
+			<p>Please email us if you keep getting sent to a invalid page</p>`)
+	}
 }
 
 func main() {
