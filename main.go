ea6782d3e0a9e53cf3de38488a875a9e949a710e
Dong Dao <kwalitycontrol@gmail.com>
2021-05-20 21:46:55 +0900

    add contact page

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func handleFunc(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my super awesome site</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my super awesome site</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:suppoer@lenslockedlcom\">support@lenslock.com</a>")
+	} 
 }
 
 func main() {
