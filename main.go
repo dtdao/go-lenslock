2747ba3170829b404f374c54b8b00a2a01be4f16
Dong Dao <kwalitycontrol@gmail.com>
2021-05-21 22:25:08 +0900

    add faq and 404

--- main.go
+++ main.go
@@ -18,10 +18,25 @@ func contact(w http.ResponseWriter, r *http.Request){
 
 }
 
+func faq(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<div><h1>FAQ</h1><p>What is this going on?</p></div>")
+}
+
+func notFound() http.Handler {
+	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request)  {
+		w.WriteHeader(http.StatusNotFound)
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, "<h1>Not Found: 404</h1>")
+	})
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = notFound()
 	http.ListenAndServe(":3000", r)
 }
 
