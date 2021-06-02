deb6cba6665009f55a807c489b7f2677290d7edc
Dong Dao <kwalitycontrol@gmail.com>
2021-06-02 23:08:31 +0900

    Add formparsers with gorilla/schema

--- controllers/users.go
+++ controllers/users.go
@@ -4,11 +4,17 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/schema"
 	"lenslocked.com/views"
 )
 
 type Users struct {
 	NewView *views.View
+}
+
+type SignupForm struct {
+	Email string `schema:"email"`
+	Password string `schema:"password"`
 }
 
 func NewUsers() *Users{
@@ -21,13 +27,23 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request)  {
 	u.NewView.Render(w, nil)
 }
 
+
 func (u *Users) Create(w http.ResponseWriter, r *http.Request){
 	if err := r.ParseForm(); err != nil {
 		panic(err)
 	}
 
-	fmt.Fprintln(w, r.PostForm["email"])
-	fmt.Fprintln(w, r.PostForm["password"])
+	dec := schema.NewDecoder()
+
+	var form SignupForm
+
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
+
+	// fmt.Fprintln(w, r.PostForm["email"])
+	// fmt.Fprintln(w, r.PostForm["password"])
 
 	fmt.Fprintln(w, "This is a fake messgae. Presend that we created a user")
 }
