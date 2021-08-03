428fce71a002d4a771bf9624c55942aa4131b8c3
Dong Dao <kwalitycontrol@gmail.com>
2021-08-03 22:15:15 +0900

    Cookie test

--- main.go
+++ main.go
@@ -51,6 +51,7 @@ func main() {
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	r.HandleFunc("/login", usersC.Login).Methods("POST")
+	r.HandleFunc("/cookietest", usersC.CookieTest).Methods("GET")
 	http.ListenAndServe(":3000", r)
 }
 
