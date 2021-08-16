cf60afc982b98f7a6e65c9fc2ad54d52940cded7
Dong Dao <kwalitycontrol@gmail.com>
2021-08-16 19:51:39 +0900

    Created rand package, to create random string and remember token

--- exp/main.go
+++ exp/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-
-	"lenslocked.com/models"
+	"lenslocked.com/rand"
 )
 
 const (
@@ -14,68 +13,73 @@ const (
 	dbname = "lenslocked_dev"
 )
 
-func main() {
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
- host, port, user, password, dbname)
-
-
-	us, err := models.NewUserService(psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-
-	us.DestructiveReset()
-	user := models.User {
-		Name: "Michael Scott",
-		Email:  "michael@dunermifflin.com",
-	}
-	if err := us.CreateUser(&user); err != nil {
-		panic(err)
-	}
-	
-	user.Email = "michael@michaelscottpaperco.com"
-	if err := us.Update(&user); err != nil {
-		panic(err)
-	}
-
-	// userByEmail, err := us.ByEmail("michael@michaelscottpaperco.com") 
-	// userById, err := us.ById(user.ID) 
-
-	if err := us.Delete(user.ID); err != nil {
-		panic(err)
-	}
-
-	userById, err := us.ById(user.ID)
-	if err != nil {
-		panic(err)
-	}
-
-
-
-	// fmt.Println(userByEmail)
-	fmt.Println(userById)
-
-	// t, err := template.ParseFiles("hello.gohtml")
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// data := struct {
-	// 	Name   string
-	// 	Place  string
-	// 	Time   int
-	// 	Nested struct {
-	// 		Name  string
-	// 		Level int
-	// 	}
-	// }{Name: "John Smith", Place: "Tokyo", Nested: struct {
-	// 	Name  string
-	// 	Level int
-	// }{"TEST", 3}}
-
-	// err = t.Execute(os.Stdout, data)
-	// if err != nil {
-	// 	panic(err)
-	// }
+func main(){
+	fmt.Println(rand.String(10))
+	fmt.Println(rand.RememberToken())
 }
+
+//func main() {
+//
+//	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+// host, port, user, password, dbname)
+//
+//
+//	us, err := models.NewUserService(psqlInfo)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	us.DestructiveReset()
+//	user := models.User {
+//		Name: "Michael Scott",
+//		Email:  "michael@dunermifflin.com",
+//	}
+//	if err := us.CreateUser(&user); err != nil {
+//		panic(err)
+//	}
+//
+//	user.Email = "michael@michaelscottpaperco.com"
+//	if err := us.Update(&user); err != nil {
+//		panic(err)
+//	}
+//
+//	// userByEmail, err := us.ByEmail("michael@michaelscottpaperco.com")
+//	// userById, err := us.ById(user.ID)
+//
+//	if err := us.Delete(user.ID); err != nil {
+//		panic(err)
+//	}
+//
+//	userById, err := us.ById(user.ID)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//
+//
+//	// fmt.Println(userByEmail)
+//	fmt.Println(userById)
+//
+//	// t, err := template.ParseFiles("hello.gohtml")
+//	// if err != nil {
+//	// 	panic(err)
+//	// }
+//
+//	// data := struct {
+//	// 	Name   string
+//	// 	Place  string
+//	// 	Time   int
+//	// 	Nested struct {
+//	// 		Name  string
+//	// 		Level int
+//	// 	}
+//	// }{Name: "John Smith", Place: "Tokyo", Nested: struct {
+//	// 	Name  string
+//	// 	Level int
+//	// }{"TEST", 3}}
+//
+//	// err = t.Execute(os.Stdout, data)
+//	// if err != nil {
+//	// 	panic(err)
+//	// }
+//}
